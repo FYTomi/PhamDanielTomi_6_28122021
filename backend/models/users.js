e5b89e27684e988094f08d71f2ad0ed186c7aff0
Pham Daniel <tomiyoshida75@gmail.com>
2022-01-13 12:48:33 +0100

    installation et application validateur unique

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,9 +1,17 @@
-const mongoose = require('mongoose');
+const mongosse = require ('mongoose');
 
-const usersSchema = mongoose.Schema({
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    
-});
+//Ajout plugin validator unique
+const uniqueValidator = require ('mongoose-unique-validator');
 
-module.exports = mongoose.model('Users', usersSchema);
+// unique: true pour s'assurer qu'on ne peut s'inscrire une fois avec la même adresse et mdp
+const userSchema = mongosse.Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+})
+
+//Le validateur doit être appliqué avant l'exportation en model
+
+userSchema.plugin(uniqueValidator);
+
+//Export du schéma sous forme de model
+module.exports = mongoose.model('User', userSchema);
