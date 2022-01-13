bdf4f35c1da8206306f5723cc09d5c834a191897
Pham Daniel <tomiyoshida75@gmail.com>
2022-01-13 22:02:23 +0100

    routes sauces

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -11,10 +11,10 @@ const sauceController = require ('../controllers/sauce');
    //Suppression d'une sauce existante
    //Récup d'une sauce spécifique, ":" pour indiquer à Express que cette partie de la route est dynamique
    //Récup toutes les sauces
-   router.post('/',auth, sauceController.createThing);
-   router.put('/:id',auth, sauceController.modifyThing );
-   router.delete('/:id',auth, stuffController.deleteThing)
-   router.get('/:id',auth, sauceController.getOneThing);
-   router.get('/',auth, sauceController.getAllThings);
+   router.post('/',auth, sauceController.createSauce);
+   router.put('/:id',auth, sauceController.modifySauce );
+   router.delete('/:id',auth, sauceController.deleteSauce)
+   router.get('/:id',auth, sauceController.getOneSauce);
+   router.get('/',auth, sauceController.getAllSauces);
  
    module.exports = router;
