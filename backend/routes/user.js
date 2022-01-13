ea8aba89baf511b8f752dfd24f7286313505bc83
Pham Daniel <tomiyoshida75@gmail.com>
2022-01-13 16:01:58 +0100

    Correction router post

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,9 +1,9 @@
 const express = require ('express'); 
 const router = express.Router();
-
+//Controleur pour associer les fonction aux différentes routes
 const userCtrl = require('../controllers/user');
 
-router.post('/signup', userCtrlSignUp);
-router.post('/login', userCtrlLogin);
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
 
 module.exports = router;
