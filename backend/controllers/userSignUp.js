f9023299a81f8d61fd0f6bb040418a20a9374b0f
Pham Daniel <tomiyoshida75@gmail.com>
2022-01-13 13:18:49 +0100

    création users

--- backend/controllers/userSignUp.js
+++ backend/controllers/userSignUp.js
@@ -1,3 +1,5 @@
+const User  = require ('../models/Users');
+
 //Middleware pour l'enregistrement de nouveau utilisateur
 exports.signup = (req, res, next) => {
 
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const userControllerLogin = require('../controllers/userLogin');
 const userControllerSignUp = require('../controllers/userSignUp');
 
-router.post()
+router.post('/signup', userControllerSignUp);
+router.post('/login', userControllerLogin);
 
 module.exports = router;
