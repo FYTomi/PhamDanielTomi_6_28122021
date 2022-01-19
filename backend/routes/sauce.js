21fda7a47eaff85a4191770502a45efcd56ef36a
Pham Daniel <tomiyoshida75@gmail.com>
2022-01-20 00:20:54 +0100

    Comment

--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -6,11 +6,11 @@ const auth= require('../middleware/auth')
 
 const sauceController = require ('../controllers/sauce');
 
-//Middleware dont le console.log indique les détails de la sauce
-   //Mettre à jour une sauce existante
-   //Suppression d'une sauce existante
-   //Récup d'une sauce spécifique, ":" pour indiquer à Express que cette partie de la route est dynamique
-   //Récup toutes les sauces
+   //POST - Middleware dont le console.log indique les détails de la sauce
+   //PUT - Mettre à jour une sauce existante
+   //DELETE - Suppression d'une sauce existante
+   //GET -  Récup d'une sauce spécifique, ":" pour indiquer à Express que cette partie de la route est dynamique
+   //GET - Récup toutes les sauces
    router.post('/',auth, sauceController.createSauce);
    router.put('/:id',auth, sauceController.modifySauce );
    router.delete('/:id',auth, sauceController.deleteSauce)
