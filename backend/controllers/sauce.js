b20f1dc09261ea8c40423d33ead65ed09b40003b
Pham Daniel <tomiyoshida75@gmail.com>
2022-01-20 16:52:58 +0100

    Route delete

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,4 +1,5 @@
 const Sauce = require ('../models/Sauces');
+const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce)
@@ -27,21 +28,14 @@ exports.modifySauce = (req, res, next)=>{
 exports.deleteSauce = (req, res, next)=>{
     Sauce.findOne({ _id: req.params.id})
     .then((sauce)=>{
-      if(!sauce){
-        return res.status(404).json({
-          error: new Error ('Sauce non trouvé')
-        });
-      }
-      if (sauce.userId !== req.auth.userId){
-        return res.status(401).json({
-          error: new Error('Requête non autorisé')
-        })
-      }
-      Sauce.deleteOne({_id: req.params.id })
-    .then(() => res.status(200).json({message : 'Sauce Supprimé'}))
-    .catch(error => res.status(400).json({ error }));
-    });
-    
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`,()=>{
+        Sauce.deleteOne({_id: req.params.id })
+          .then(() => res.status(200).json({message : 'Sauce Supprimé'}))
+          .catch(error => res.status(400).json({ error }))  
+      })
+    })
+    .catch((error) => res.status(500).json({ error }))
   };
 
 exports.getOneSauce = (req, res, next)=>{
