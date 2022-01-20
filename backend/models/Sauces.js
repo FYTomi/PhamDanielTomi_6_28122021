fa773d59fcc513b728a994e84fe53667c5da534c
Pham Daniel <tomiyoshida75@gmail.com>
2022-01-20 12:19:44 +0100

    Modif model

--- backend/models/Sauces.js
+++ backend/models/Sauces.js
@@ -6,12 +6,12 @@ const sauceSchema = mongoose.Schema({
         manufacturer : {type: String, required: true },
         description : {type: String, required: true },
         mainPepper : {type: String, required: true },
-        imageUrl : {type: String, required:true},
+        imageUrl : {type: String, required: true},
         heat: {type: Number, required: true },
-        likes : {type: Number, required: true },
-        dislikes : {type: Number, required: true },
-        usersLiked : { type : [String]},
-        usersDisliked: { type: [String] }
+        likes : {type: Number, required: false, default: 0 },
+        dislikes : {type: Number, required: false, default: 0 },
+        usersLiked : { type : [String], required: false},
+        usersDisliked: { type: [String], required: false }
 })
     
 
